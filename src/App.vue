40299eb33e653b82f0344f5bdccc9ac9ed6bfb2e
Jeyson Murillo <jeysonmurillo@Jeysons-iMac.local>
2020-03-13 17:14:25 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-show' }">Show</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
@@ -24,5 +23,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#nav {
+  padding: 30px;
 }
 </style>
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Router from "vue-router";
+import EventCreate from "./views/EventCreate";
+import EventList from "./views/EventList";
+import EventShow from "./views/EventShow";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    { path: "/", name: "event-list", component: EventList },
+    { path: "/event", name: "event-show", component: EventShow },
+    { path: "/event/create", name: "event-create", component: EventCreate }
+  ]
+});
